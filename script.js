25563b6e55ccd1940f93c4a9c61b05d52f7e417e
Brian Dixon <briandixon@Brians-Mac-mini.home>
2018-08-02 19:56:16 -0400

    added comments

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ const tracker = new tracking.ObjectTracker('face')
 const nikeButton = document.getElementById('nike')
 const adidasButton = document.getElementById('adidas')
 
+////make a new img object that can have its size made base on the picture
+///we need to render
 const img = new Image()
 let filterX = 0
 let filterY = 0
 let filterWidth = 0
 let filterHeight = 0
 
+///uses our Image instance to create a pic
 function changePic (x, y, width, height, src) {
   img.src = src
   filterX = x
@@ -22,26 +25,33 @@ function changePic (x, y, width, height, src) {
 }
 
 //////these are hard codded values for the symbol
-////we will test with a flower crown image
+////we can test with 2 images. the first is a flower crown image
+///with these coordinates changePic(0,-0.5, 1,1)
+///for bunny ears its -0.5, -0.9, 2, 2
 function nike () {
-  changePic(0, 0.95, 1, 1, 'nike.jpg')
+  changePic(0, 0.98, 1, 1, 'nike.jpg')
 }
 
 nike()
-
+//------buttons----------
+//these are the only 2 buttons, i did not feel like there was a need to make a
+///seperate function for adidas picture formating
+///parameters for test photo
 nikeButton.addEventListener('click', nike)
 /////tested with bunny ears image
 adidasButton.addEventListener('click', () => {
-  changePic(-0.5, -0.9, 2, 2, 'bunny-ears.png')
+  changePic(-0.5, -0.9, 2, 2, 'adidas.jpg')
 })
 
+///sets the trackers initial values for detection. Values based on
+///experimenting with my face
 tracker.setInitialScale(4)
 tracker.setStepSize(2)
 tracker.setEdgesDensity(0.1)
 tracking.track('#video', tracker, { camera: true })
 
 
-//////make sure that
+//////make sure that image is drawn over video
 tracker.on('track', event => {
   context.clearRect(0, 0, canvas.width, canvas.height)
   event.data.forEach(rect => {
